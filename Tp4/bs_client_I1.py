a2ff011c2591b5fcf08db3f7ff03fb1f97b5aba7
Logards <bastien.ricci@ynov.com>
2023-11-06 10:31:44 +0100

    tp4 server and client

--- Tp4/bs_client_I1.py
+++ Tp4/bs_client_I1.py
@@ -9,9 +9,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connexion au serveur
 s.connect((host, port))
-
+arr = bytes('MEOOOOOOOO', 'utf-8')
 # Envoi de data bidon
-s.sendall('MEO')
+s.sendall(arr)
 
 # On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
 data = s.recv(1024)
