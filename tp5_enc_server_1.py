80aacc641c84a64ec7c8d519b36eef234a55bcfc
Logards <bastien.ricci@ynov.com>
2023-11-30 10:51:58 +0100

    essai

--- tp5_enc_server_1.py
+++ tp5_enc_server_1.py
@@ -1,26 +1,42 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 13337))  
-s.listen(1)
-client, client_addr = s.accept()
-
-s.listen(1)
-conn, addr = s.accept()
-print("salut a toi")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('10.1.1.11', 13337))
+sock.listen()
+client, client_addr = sock.accept()
 
 while True:
-    header = conn.recv(4)
-    if not header: break
+    # On lit les 4 premiers octets qui arrivent du client
+    # Car dans le client, on a fixé la taille du header à 4 octets
+    header = client.recv(4)
+    if not header:
+        break
+
+    # On lit la valeur
     msg_len = int.from_bytes(header[0:4], byteorder='big')
+
     print(f"Lecture des {msg_len} prochains octets")
+
+    # Une liste qui va contenir les données reçues
     chunks = []
+
     bytes_received = 0
     while bytes_received < msg_len:
-        chunks = client.recv(min(msg_len - bytes_received, 1024)) 
-        if not chunks: raise RuntimeError("Invalid chunk received sussy baka")
-        chunks.append(chunks)
-        bytes_received = len(chunks)
-    message_received =  b"".join(chunks).decode('utf-8')
+        # Si on reçoit + que la taille annoncée, on lit 1024 par 1024 octets
+        chunk = client.recv(min(msg_len - bytes_received,
+                                1024))
+        if not chunk:
+            raise RuntimeError('Invalid chunk received bro')
+
+        # on ajoute le morceau de 1024 ou moins à notre liste
+        chunks.append(chunk)
+
+        # on ajoute la quantité d'octets reçus au compteur
+        bytes_received += len(chunk)
+
+    # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
+    message_received = b"".join(chunks).decode('utf-8')
     print(f"Received from client {message_received}")
-conn.close()
+
+client.close()
+sock.close()
