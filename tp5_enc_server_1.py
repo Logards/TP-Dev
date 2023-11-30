05afef820dbc7a36f1f55985d41c420865683614
Logards <bastien.ricci@ynov.com>
2023-11-30 11:07:44 +0100

    essai

--- tp5_enc_server_1.py
+++ tp5_enc_server_1.py
@@ -37,8 +37,8 @@ while True:
     # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
     message_received = b"".join(chunks).decode('utf-8')
     print(f"Received from client {message_received}")
-    print(type(message_received))
     result = eval(message_received)
+    print(type(result))
     sock.send(str(message_received).encode("utf-8"))
 
 client.close()
