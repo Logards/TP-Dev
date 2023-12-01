6022e788ec2bedb75393786174a28be587dc5d86
Logards <bastien.ricci@ynov.com>
2023-12-01 09:39:13 +0100

    Fix socket connection and send encoded message

--- tp5_enc_client_2.py
+++ tp5_enc_client_2.py
@@ -1,8 +1,8 @@
 import socket
 import re
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.connect(('10.1.1.11', 13337))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('10.1.1.11', 13337))
 def int_to_bytes(integer):
     return integer.to_bytes((integer.bit_length() + 7) // 8, 'big')
 
@@ -23,8 +23,8 @@ msg_encode = int_to_bytes(int(number[0]))
 msg_encode += operator[0].encode()
 msg_encode += int_to_bytes(int(number[1]))
 print(msg_encode)
-#s.send(payload)
-#s_data = s.recv(1024)
-#print(s_data.decode())
-#s.close()
+s.send(msg_encode)
+s_data = s.recv(1024)
+print(s_data.decode())
+s.close()
 
