0fe2083dc2692dcc63c6105524f620c2744254fa
Logards <bastien.ricci@ynov.com>
2023-12-01 10:15:01 +0100

    Fix socket connection issue and close connection properly

--- tp5_web_serv_1.py
+++ tp5_web_serv_1.py
@@ -3,8 +3,8 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('10.1.1.11', 13337))
 sock.listen()
-conn, addr = sock.accept()
 while True:
+    conn, addr = sock.accept()
     try:
         data = conn.recv(1024)
         if not data: break
@@ -12,10 +12,12 @@ while True:
         if "GET /" in data.decode():
             envoie = "HTTP/1.0 200 OK\n\n<h1>Hello je suis un serveur HTTP</h1>".encode()
             conn.send(envoie)
+        conn.close()
     
     except socket.error:
         print ("Error Occured.")
         break
-conn.close()
+    finally:
+        conn.close()
 sock.close()
 exit()
