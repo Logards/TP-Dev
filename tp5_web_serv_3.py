c901679ef79abf8187323ae4edd465e4b2d60bca
Logards <bastien.ricci@ynov.com>
2023-12-01 10:35:40 +0100

    Add new HTML file and Python script for web server

---
+++ tp5_web_serv_3.py
@@ -0,0 +1,27 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('10.1.1.11', 13337))
+sock.listen()
+while True:
+    conn, addr = sock.accept()
+    try:
+        data = conn.recv(1024)
+        if not data: break
+        print(f"Donnéees reçues du client : {data}")
+
+        if "GET /toto.html" in data.decode():
+            file = open('toto.html')
+            html_content = file.read()
+            file.close()
+            http_reponse = 'HTTP/1.0 200 OK\n\n' + html_content
+            conn.send(http_reponse.encode())
+        conn.close()
+    
+    except socket.error:
+        print ("Error Occured.")
+        break
+    finally:
+        conn.close()
+sock.close()
+exit()
